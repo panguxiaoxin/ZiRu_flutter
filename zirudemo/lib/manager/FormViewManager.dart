395e648119b9fd7af05ad55ef80164265d7d1522
panguxiaoxin <972786779@qq.com>
2020-01-08 16:07:41 +0800

    openform

---
+++ zirudemo/lib/manager/FormViewManager.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class FormViewManager {
+  openform(BuildContext context, String url, String strTitle, String strData,
+      int nOpenMode, int nAnimation) {}
+}
